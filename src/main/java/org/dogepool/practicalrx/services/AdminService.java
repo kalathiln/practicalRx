8f237917889358851c30b039c274be1c6b6f74e5
kalathiln <nikhkk@gmail.com>
2017-11-02 16:08:51 +0100

    Migration to RxJava on a Spring Rest API.

--- src/main/java/org/dogepool/practicalrx/services/AdminService.java
+++ src/main/java/org/dogepool/practicalrx/services/AdminService.java
@@ -3,6 +3,9 @@ package org.dogepool.practicalrx.services;
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.Month;
+import io.reactivex.Observable;
+
+
 
 import org.springframework.stereotype.Service;
 
@@ -12,18 +15,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class AdminService {
 
-    public BigInteger costForMonth(int year, Month month) {
+//    public BigInteger costForMonth(int year, Month month) {
+//        LocalDate now = LocalDate.now();
+//
+//        if (year == now.getYear() && month == now.getMonth()) {
+//            return BigInteger.ZERO;
+//        }
+//        if (year > now.getYear()
+//            || year == now.getYear() && month.getValue() > now.getMonthValue()) {
+//            return BigInteger.ZERO;
+//        }
+//        return BigInteger.valueOf(
+//                year +
+//                month.getValue() * 100);
+//    }
+	
+	/* Converting the above Spring code into RxJava
+	 * Using factory method Observable.just() and the map operator.
+	 * 
+	 */
+    
+	public Observable<BigInteger> costForMonth(int year, Month month) {
         LocalDate now = LocalDate.now();
 
         if (year == now.getYear() && month == now.getMonth()) {
-            return BigInteger.ZERO;
+            return Observable.just(BigInteger.ZERO);
         }
         if (year > now.getYear()
             || year == now.getYear() && month.getValue() > now.getMonthValue()) {
-            return BigInteger.ZERO;
+            return Observable.just(BigInteger.ZERO);
         }
-        return BigInteger.valueOf(
-                year +
-                month.getValue() * 100);
+        return Observable.just(month.getValue())
+        		.map(v -> year + v * 100)
+        		.map(integer -> BigInteger.valueOf(integer));
     }
 }
